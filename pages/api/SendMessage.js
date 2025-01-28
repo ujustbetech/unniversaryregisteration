704faedd1e2c8aacb57f7e2f227ad4cf84a36c5a
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2025-01-28 12:38:35 +0530

    Update SendMessage.js

--- pages/api/SendMessage.js
+++ pages/api/SendMessage.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { phoneNumbers, userDetails } = req.body; // Accept phone numbers and user details in request body
-    const token = process.env.WHATSAPP_API_TOKEN;
+    const token = "EAAHwbR1fvgsBOZBLeLD7wdbDO7sOGgYPYJ3z6c9dlSl8VflexOPOWP2JcKGCLUHIo7r7TfIfLIJCbnek7VprT8ZBTH7iYL8CjPZCWjT9u5tmNaRw71e9vs3u0nAn6VeAVHJLCveIM8ZBZChT9loyWtya5ZB94IgG2oGzqYS5FHeTgxbAMAjE55SgMIFJR8QzFKGUXpYiCTpCYeLx4S1T8rKFyAA3aZCQEBLPQm6ZAxJw";
 
     // Validate input
     if (!phoneNumbers || !Array.isArray(phoneNumbers) || phoneNumbers.length === 0) {
