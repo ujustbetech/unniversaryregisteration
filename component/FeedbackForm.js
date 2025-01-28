69981f5af88056bdc05863b6385c377297ca89f7
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2025-01-28 15:32:28 +0530

    Update FeedbackForm.js

--- component/FeedbackForm.js
+++ component/FeedbackForm.js
@@ -5,6 +5,7 @@ import { doc, updateDoc } from 'firebase/firestore';
 import Swal from 'sweetalert2';
 import "../src/app/styles/main.scss";
 import "../pages/feedback.css";
+import "../pages/event.css"
 
 const FeedbackForm = () => {
   const router = useRouter();
@@ -90,44 +91,49 @@ const FeedbackForm = () => {
   };
 
   return (
-    <section className="feedback-form-container">
-      <h2 className="feedback-form-title">Feedback Form</h2>
-      <form onSubmit={handleSubmit}>
-        <ul className="feedback-questions">
-          {questions.map((question) => (
-            <li className="feedback-question-row" key={question.id}>
+    <section className='feedbackContainer'>
+    <div className='feedback_logo'>
+          <img src="/Universary.png" alt="Logo" />
+        </div>
+      <div className="feedback-form-container">
+        <h2 className="feedback-form-title">Feedback Form</h2>
+        <form onSubmit={handleSubmit}>
+          <ul className="feedback-questions">
+            {questions.map((question) => (
+              <li className="feedback-question-row" key={question.id}>
+                <div className="feedback-options">
+                  <h3 className="feedback-question-title">{question.text}</h3>
+                  {question.options.map((option, i) => (
+                    <label key={i}>
+                      <input
+                        type="checkbox"
+                        value={option}
+                        checked={feedback[question.id].includes(option)}
+                        onChange={() => handleChange(question.id, option)}
+                      />
+                      {option}
+                    </label>
+                  ))}
+                </div>
+              </li>
+            ))}
+            <li className="feedback-question-row">
               <div className="feedback-options">
-                <h3 className="feedback-question-title">{question.text}</h3>
-                {question.options.map((option, i) => (
-                  <label key={i}>
-                    <input
-                      type="checkbox"
-                      value={option}
-                      checked={feedback[question.id].includes(option)}
-                      onChange={() => handleChange(question.id, option)}
-                    />
-                    {option}
-                  </label>
-                ))}
+                <h3 className="feedback-question-title">Additional Comments</h3>
+                <textarea
+                  className="feedback-comment-box"
+                  value={feedback.comment}
+                  onChange={handleCommentChange}
+                  placeholder="Write your comments here"
+                />
               </div>
             </li>
-          ))}
-          <li className="feedback-question-row">
-            <div className="feedback-options">
-              <h3 className="feedback-question-title">Additional Comments</h3>
-              <textarea
-                className="feedback-comment-box"
-                value={feedback.comment}
-                onChange={handleCommentChange}
-                placeholder="Write your comments here"
-              />
-            </div>
-          </li>
-        </ul>
-        <button className="submitbtns" type="submit">
-          Submit
-        </button>
-      </form>
+          </ul>
+          <button className="submitbtns" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
